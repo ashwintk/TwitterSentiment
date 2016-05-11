d36858790469b205ed9d65985fe592574906bd74
Ashwin Kumar <tashwin@okstate.edu>
2016-05-11 14:44:45 -0500

    Completed the following
    # 1. Remove dURL's and user mentions.
    # 2. Replaced new line character (\n) with space and return carriage character (\r) with ''
    # 4. Replaced hash tags with words
    # 5. Replaced emoticons with emotions (positive, negative and neutral)
    # 6. Stripped all punctuations other than space, apostrophe and hyphen
    # 7. Replaced Internet slang words with complete words
    # 8. Replaced multiple white spaces with a single space
    
    Extracted only timestamp, userhandle and tweet text from tweets

--- ManipulateTweets.py
+++ ManipulateTweets.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import re
+import re, csv, sys
 
 # This function is used to remove all the URL's in a tweet
 def removeURL(tweetText):
@@ -40,12 +40,97 @@ def replaceRepeatedCharacters(tweetText):
 
 # Filter tweets other than passed language
 def CheckLanguage(tweetLanguage,langFilter):
-    #Check for Rule 1 - Tweet words are in Group1 only
     if tweetLanguage.strip().lstrip().lower()==langFilter.strip().lstrip().lower():
         return True
     else:
         return False
+
 # This function removes punctuations from tweet text
 def stripTweet(tweetText):
-    tweetText=tweetText.strip('\'"')
-    return tweetText.strip('\'"?,.')
+    tweetText=tweetText.strip('"-')
+    return tweetText.strip('"?,.')
+
+# This function is used to convert multiple white spaces into a single white space
+def convertMultipleWhiteSpacesToSingleWhiteSpace(tweetText):
+    return re.sub('[\s]+', ' ', tweetText)
+
+# This function can be used to parse a file given a delimiter, file name and key & value to select
+def readFileandReturnADict(fileName, readMode, delimiter, keyIndex, valueIndex, isLower, fileDialect = None):
+    mydict={}
+    with open(fileName, readMode) as readHandle:
+        if fileDialect is None:
+            fileDialect = csv.Sniffer().sniff(readHandle.read(), delimiters=delimiter)
+        readHandle.seek(0)
+        reader = csv.reader(readHandle, fileDialect)
+        for line in reader:
+            if isLower:
+                mydict.update({line[keyIndex].lower(): line[valueIndex]})
+            else:
+                mydict.update({line[keyIndex]: line[valueIndex]})
+    readHandle.close()
+    return mydict
+
+# This function splits a string by a character and replaces words from key of the dictionary
+def replaceOccurrencesOfAString(text,splitBy,dict,lowerFlag):
+    str_array=text.split(splitBy)
+    return_str=""
+    for word in str_array:
+        key_to_Search=str(word).strip().lstrip()
+        if len(key_to_Search) >1:
+            if lowerFlag:
+                key_to_Search=word.lower()
+            if dict.has_key(key_to_Search):
+                return_str+=" "+dict[key_to_Search]
+            else:
+                return_str += " " + word
+    return return_str
+
+
+# This function can be used to replace emoticons with their emotions (POSITIVE/NEGATIVE/NEUTRAL)
+# A complete list of emoticons are obtained from wikipedia.org/wiki/List_of_emoticons
+def replaceEmoticons(tweet_texts, splitBy,lowerFlag):
+    str_array = tweet_texts.split(splitBy)
+    return_str = ""
+    for word in str_array:
+        wordToFind = str(word).strip().lstrip()
+        if lowerFlag:
+            wordToFind = word.lower()
+        if (wordToFind == ":-)" or wordToFind == ":)" or wordToFind == ":D" or wordToFind == ":o)" or wordToFind == ":]" or wordToFind == ":3" or
+                    wordToFind == ":c)" or wordToFind == ":>" or wordToFind == "=]" or wordToFind == "8)" or wordToFind == "=)" or
+                    wordToFind == ":}" or wordToFind == ":^)" or wordToFind == ":-D" or wordToFind == "8-D" or wordToFind == "8D" or
+                    wordToFind == "x-D" or wordToFind == "xD" or wordToFind == "X-D" or wordToFind == "XD" or wordToFind == "=-D" or
+                    wordToFind == "=D" or wordToFind == "=-3" or wordToFind == "=3" or wordToFind == "B^D" or wordToFind == ":-))" or
+                    wordToFind == ":'-)" or wordToFind == ":')" or wordToFind == ":*" or wordToFind == ":-*" or wordToFind == ":^*" or
+                    wordToFind == "( '}{' )" or wordToFind == ";-)" or wordToFind == ";)" or wordToFind == "*-)" or wordToFind == "*)" or
+                    wordToFind == ";-]" or wordToFind == ";]" or wordToFind == ";D" or wordToFind == ";^)" or wordToFind == ":-," or
+                    wordToFind == ">:P" or wordToFind == ":-P" or wordToFind == ":P" or wordToFind == "X-P" or wordToFind == "x-p" or
+                    wordToFind == "xp" or wordToFind == "XP" or wordToFind == ":-p" or wordToFind == ":p" or wordToFind == "=p" or
+                    wordToFind == ":-p" or wordToFind == ":p" or wordToFind == ":p" or wordToFind == ":-p" or wordToFind == ":-b" or
+                    wordToFind == ":b" or wordToFind == "d:" or wordToFind == "O:-)" or wordToFind == "0:-3" or wordToFind == "0:3" or
+                    wordToFind == "0:-)" or wordToFind == "0:)" or wordToFind == "0;^)" or wordToFind == "o/\o" or wordToFind == "^5" or
+                    wordToFind == ">_>^" or wordToFind == "^<_<" or wordToFind == "[;-)" or wordToFind == "#-)" or wordToFind == "\o/" or
+                    wordToFind == "*\0/*" or wordToFind == "<3"):
+            return_str += " POSITIVE"
+        elif (wordToFind == ">:[" or wordToFind == ":-(" or wordToFind == ":(" or wordToFind == ":-c" or wordToFind == ":c" or
+                      wordToFind == ":-<" or wordToFind == ":<" or wordToFind == ":-[" or wordToFind == ":[" or
+                      wordToFind == ":{" or wordToFind == ":-||" or wordToFind == ":@" or wordToFind == ">:(" or
+                      wordToFind == ":'-(" or wordToFind == ":'(" or wordToFind == "D:<" or wordToFind == "D:" or
+                      wordToFind == "D8" or wordToFind == "D;" or wordToFind == "D=" or wordToFind == "DX" or
+                      wordToFind == "v.v" or wordToFind == "D-':" or wordToFind == ">:\\" or wordToFind == ">:/" or
+                      wordToFind == ":-/" or wordToFind == ":-." or wordToFind == ":/" or wordToFind == ":\\" or
+                      wordToFind == "=/" or wordToFind == "=\\" or wordToFind == ":L" or wordToFind == "=L" or
+                      wordToFind == ":S" or wordToFind == ">.<" or wordToFind == ":$" or wordToFind == ">:)" or
+                      wordToFind == ">;)" or wordToFind == ">:-)" or wordToFind == "}:-)" or wordToFind == "}:)" or
+                      wordToFind == "3:-)" or wordToFind == "3:)" or wordToFind == "|-O" or wordToFind == ":-&" or
+                      wordToFind == ":&" or wordToFind == "%)" or wordToFind == ":-###.." or wordToFind == ":###.." or
+                      wordToFind == "<:-<" or wordToFind == "<*)))-{" or wordToFind == "><(((*>" or wordToFind == "><>" or wordToFind == "</3"):
+            return_str += " NEGATIVE"
+        elif (wordToFind == ";{" or wordToFind == ">:O" or wordToFind == ":-O" or wordToFind == ":O" or wordToFind == ":-o" or
+                      wordToFind == ":o" or wordToFind == "8-0" or wordToFind == "O_O" or wordToFind == "o-o" or wordToFind == "O_o" or
+                      wordToFind == "o_O" or wordToFind == "o_o" or wordToFind == "O-O" or wordToFind == ":|" or wordToFind == ":-|" or
+                      wordToFind == ":-X" or wordToFind == ":X" or wordToFind == ":-#" or wordToFind == ":#" or wordToFind == ":-J" or
+                      wordToFind == "%-)"):
+            return_str += " NEUTRAL"
+        else:
+            return_str += " " + wordToFind
+    return return_str
--- Task_1/ExtractInfoFromTweetsMapper.py
+++ Task_1/ExtractInfoFromTweetsMapper.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
-import json, sys
+import json, sys, re
 
 sys.path.append('./')
 
 import ManipulateTweets as manip
-#This mapper reads the twitter data and extracts only the following information
+
+# This mapper reads the twitter data and extracts only the following information
 # 1. Data Created
 # 2. User Handle
 # 3. Tweet text
@@ -13,6 +14,26 @@ import ManipulateTweets as manip
 # This mapper also performs the following operations on the tweet text.
 # 1. Remove URL's and user mentions.
 # 2. Replaces new line character (\n) with space and return carriage character (\r) with ''
+# 4. Replaces hash tags with words
+# 5. Replaces emoticons with emotions (positive, negative and neutral)
+# 6. Strips all punctuations other than space, apostrophe and hyphen
+# 7. Replaces Internet slang words with complete words
+# 8. Replaces multiple white spaces with a single space
+
+
+
+# A complete list of internet slang words are obtained from noslang.com
+# This file is stored in CSV format and passed as a side-effect
+# Following code loads all the internet slang and their actual meaning into a dictionary
+# In this dictionary keys are slang words and values of these keys are actual meaning of these slang
+
+internet_slang_dict = manip.readFileandReturnADict("internet_slang.csv", "rb", '|', 0, 1, True,None)
+
+# A list of possible word contractions in english language are identified
+# Negative contractions of verbs are replaced by 'NOT'
+# Anything but the negative contractions are replaced with their full form
+
+contractions_dict = manip.readFileandReturnADict("Contractions.csv", "rU", ',', 0, 1, True)
 
 for line in sys.stdin:
     try:
@@ -24,16 +45,27 @@ for line in sys.stdin:
         user_handle = parsed_json_tweets['user']['screen_name'].lstrip().strip()
         # Extract tweet text
         tweet_text = parsed_json_tweets['text'].lstrip().strip()
-        # Remove new line and carriage return
-        tweet_text = manip.stripNewLineAndReturnCarriage(tweet_text)
-        # Remove URL's & User Mentions
-        tweet_text = manip.removeURL(tweet_text)
-        tweet_text = manip.removeUserMentions(tweet_text)
-        # Replace sequence of repeated characters by three characters
-        tweet_text=manip.replaceRepeatedCharacters(tweet_text)
-        # Convert multiple white spaces to a single white space
-        tweet_text = manip.convertMultipleWhiteSpacesToSingleWhiteSpace(tweet_text)
-        # Send Date created, user handle and tweet text as output
-        print '%s,%s\t%s' %(date_created,user_handle,tweet_text)
-    except(ValueError):
-        print ("JSON format error")
+        if len(tweet_text) > 1:
+            # Remove new line and carriage return
+            tweet_text = manip.stripNewLineAndReturnCarriage(tweet_text)
+            # Remove URL's & User Mentions
+            tweet_text = manip.removeURL(tweet_text)
+            tweet_text = manip.removeUserMentions(tweet_text)
+            # Replace sequence of repeated characters by three characters
+            tweet_text = manip.replaceRepeatedCharacters(tweet_text)
+            # Convert multiple white spaces to a single white space
+            tweet_text = manip.convertMultipleWhiteSpacesToSingleWhiteSpace(tweet_text)
+            # Replace hashtags with words
+            tweet_text = manip.replaceHashTagsWithWords(tweet_text)
+            # Replace all the internet slang words
+            tweet_text = manip.replaceOccurrencesOfAString(tweet_text, ' ', internet_slang_dict, True).lstrip().strip()
+            # Replace all word contractions
+            tweet_text = manip.replaceOccurrencesOfAString(tweet_text, ' ', contractions_dict, True).lstrip().strip()
+            # Replace all emoticons
+            tweet_text = manip.replaceEmoticons(str(tweet_text), ' ', False).lstrip().strip()
+            # Convert multiple white spaces to a single white space
+            tweet_text = manip.convertMultipleWhiteSpacesToSingleWhiteSpace(tweet_text)
+            # Send Date created, user handle and tweet text as output
+            print '%s,%s\t%s' % (date_created, user_handle, tweet_text)
+    except ValueError:
+        continue
